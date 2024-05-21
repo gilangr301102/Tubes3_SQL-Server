83e6ff2eeacaae67678a2bcdc71ce520aa058096
gilangr301102 <muhgilangramadhan.3011@gmail.com>
2024-05-21 18:22:05 +0700

    fix gui

--- src/frontend/gui/Program.cs
+++ src/frontend/gui/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace WindowsFormsApp
+namespace gui
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
