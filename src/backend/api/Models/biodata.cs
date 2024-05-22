5e5281273c4a2cf03103fccc8859e3d3c807079e
gilangr301102 <muhgilangramadhan.3011@gmail.com>
2024-05-22 15:27:43 +0700

    fix biodata model

--- src/backend/api/Models/biodata.cs
+++ src/backend/api/Models/biodata.cs
@@ -4,15 +4,15 @@ namespace api.Models
 {
     public class Biodata
     {
-        public string Nama { get; set; }
-        public string TempatLahir { get; set; }
-        public string TanggalLahir { get; set; }
-        public KelaminEnum JenisKelamin { get; set; }
-        public string GolonganDarah { get; set; }
-        public string Alamat { get; set; }
-        public string Agama { get; set; }
-        public StatusPerkawinanEnum StatusPerkawinan { get; set; }
-        public string Pekerjaan { get; set; }
-        public string Kewarganegaraan { get; set; }
+        public required string Nama { get; set; }
+        public required string TempatLahir { get; set; }
+        public required string TanggalLahir { get; set; }
+        public required KelaminEnum JenisKelamin { get; set; }
+        public required string GolonganDarah { get; set; }
+        public required string Alamat { get; set; }
+        public required string Agama { get; set; }
+        public required StatusPerkawinanEnum StatusPerkawinan { get; set; }
+        public required string Pekerjaan { get; set; }
+        public required string Kewarganegaraan { get; set; }
     }
 }
