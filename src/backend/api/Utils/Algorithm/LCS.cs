dc9ce774e1b0475ff548a36bba76c120698da34f
gilangr301102 <muhgilangramadhan.3011@gmail.com>
2024-05-24 00:55:44 +0700

    Fix bahasa alay checking similarity handle more alay case

--- src/backend/api/Utils/Algorithm/LCS.cs
+++ src/backend/api/Utils/Algorithm/LCS.cs
@@ -1,10 +1,13 @@
-﻿namespace api.Utils.Algorithm
+﻿using System.Text.RegularExpressions;
+using api.Utils.Helper;
+
+namespace api.Utils.Algorithm
 {
+    // Longest Common Subsequence (non-contiguous)
+    // Implemented using sum prefix with Dynamic Programming
     public static class LCS
     {
-        // Longest Common Subsequence (non-contiguous)
-        // Implemented using sum prefix with Dynamic Programming
-        public static int ComputeSimilarity(string str1, string str2)
+        public static int ComputeSimilarityCommon(string str1, string str2)
         {
             if (str1 == "" || str2 == "")
                 return 0;
@@ -27,6 +30,49 @@
                     if (str1[i - 1] == str2[j - 1])
                     {
                         dp[i, j] = dp[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        if (dp[i - 1, j] >= dp[i, j - 1])
+                            dp[i, j] = dp[i - 1, j];
+                        else
+                            dp[i, j] = dp[i, j - 1];
+                    }
+                }
+            }
+            return dp[m, n];
+        }
+
+        public static int ComputeSimilarityAlay(string str1, string str2)
+        {
+            if (str1 == "" || str2 == "")
+                return 0;
+            str1 = Regex.Replace(str1, @"[^\w\d]", "");
+            str2 = Regex.Replace(str2, @"[^\w\d]", "");
+            str2 = str2.ToLower();
+            int m = str1.Length;
+            int n = str2.Length;
+            int[,] dp = new int[m + 1, n + 1];
+            for (int i = 0; i <= m; i++) dp[i, 0] = 0;
+            for (int j = 0; j <= n; j++) dp[0, j] = 0;
+            bool[] isVisited = new bool[m + 1];
+            for (int i = 0; i <= m; i++) isVisited[i] = false;
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    char currentStr1Char = str1[i - 1];
+                    char nextStr1Char = (i < m && (str1[i - 1] == '1')) ? str1[i] : '\0';
+                    int tempTypeNormalize = CharHandler.NormalizeAlayChar(currentStr1Char, nextStr1Char, str2[j - 1]);
+                    if (tempTypeNormalize > 0 && !isVisited[i - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                        if (tempTypeNormalize == 2 && dp[i + 1, j] < dp[i, j])
+                        {
+                            dp[i + 1, j] = dp[i, j];
+                            isVisited[i] = true;
+                        }
                     }
                     else
                     {
--- src/backend/api/Utils/Helper/CharHandler.cs
+++ src/backend/api/Utils/Helper/CharHandler.cs
@@ -3,15 +3,22 @@
     public class CharHandler
     {
 
-        public static char NormalizeAlayChar(char a)
+        public static int NormalizeAlayChar(char a, char b, char c)
         {
-            if (a == '1') return 'i';
-            if (a == '4') return 'a';
-            if (a == '3') return 'e';
-            if (a == '0') return 'o';
-            if (a == '5') return 's';
-
-            return char.ToLower(a);
+            if (a == '1' && b == '3' && c == 'b') return 2;
+            if (a == '1' && b == '2' && c == 'r') return 2;
+            if (a == '1' && b == '7' && c == 'd') return 2;
+            if (a == '1' && (c == 'i' || c == 'l')) return 1;
+            if (a == '4' && c == 'a') return 1;
+            if (a == '3' && c == 'e') return 1;
+            if (a == '0' && c == 'o') return 1;
+            if (a == '5' && c == 's') return 1;
+            if (a == '6' && c == 'g') return 1;
+            if (a == '7' && (c == 't' || c == 'j')) return 1;
+            if (a == '8' && c == 'b') return 1;
+            if (a == '9' && c == 'g') return 1;
+            if (char.ToLower(a) == c) return 1;
+            return 0;
         }
     }
 }
