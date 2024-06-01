acf4958c39c822c98adf40f3d75c3745454eeb01
gilangr301102 <muhgilangramadhan.3011@gmail.com>
2024-06-01 12:04:17 +0700

    fix dataContext to get data from database

---
+++ src/backend/api/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+using System.Reflection.Emit;
+
+namespace api.Data
+{
+	public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Biodata> Biodatas { get; set; }
+        public DbSet<SidikJari> SidikJaries { get; set; }
+    }
+}
+
--- src/backend/api/Repositories/BiodataRepository.cs
+++ src/backend/api/Repositories/BiodataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using api.Data;
 using api.Models; // Assuming Biodata model is in this namespace
 // Add any additional using directives if needed to resolve assembly references
 
@@ -6,18 +7,18 @@ namespace api.Repositories
 {
     public class BiodataRepository
     {
-        //private readonly Context _context; // Ensure Context is properly referenced
+        private readonly DataContext _context; // Ensure Context is properly referenced
 
-        //public BiodataRepository(Context context)
-        //{
-        //    _context = context;
-        //}
+        public BiodataRepository(DataContext context)
+        {
+            _context = context;
+        }
 
-        //public Biodata GetBiodataByName(string name)
-        //{
-        //    // Assuming you have a DbSet<Biodata> named Biodatas in your DbContext
-        //    // This assumes the name is unique; adjust accordingly if it's not
-        //    return _context.Biodatas.FirstOrDefault(b => b.Name == name);
-        //}
+        public ICollection<Biodata> GetBiodataByName(string name)
+        {
+            // Assuming you have a DbSet<Biodata> named Biodatas in your DbContext
+            // This assumes the name is unique; adjust accordingly if it's not
+            return _context.Biodatas.Where(b => b.Nama == name).ToList();
+        }
     }
 }
