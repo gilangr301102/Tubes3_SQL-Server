75709345d01f320df12bc15fb5f44b956fa21c42
gilanglahat22 <gilanglahat43@gmail.com>
2024-06-09 23:46:45 +0700

    fix api assembly

--- src/backend/api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ src/backend/api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9334723c5323b03c1d5fd132b46a2ef572085527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e2ad35e63c229f8017165576261b9048f1d8bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
